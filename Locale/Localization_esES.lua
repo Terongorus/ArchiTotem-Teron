8959f85ebe4940cb8c09f7665d4463b3249c38ab
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2024-11-21 23:55:50 +0100

    feat: reformatted file

--- Locale/Localization_esES.lua
+++ Locale/Localization_esES.lua
@@ -13,7 +13,6 @@ L["Magma Totem"] = "Tótem de magma"
 L["Frost Resistance Totem"] = "Tótem de resistencia a la Escarcha"
 L["Flametongue Totem"] = "Tótem Lengua de fuego"
 L["Mana Spring Totem"] = "Tótem Fuente de maná"
-L["Mana Tide Totem"] = "Tótem Marea de maná"
 L["Fire Resistance Totem"] = "Tótem de resistencia al Fuego"
 L["Poison Cleansing Totem"] = "Tótem contraveneno"
 L["Disease Cleansing Totem"] = "Tótem de limpieza de enfermedades"
@@ -47,12 +46,18 @@ L["Tooltips are now turned off"] = "Tooltips are now turned off"
 L["Debuging are now turned on"] = "Debuging are now turned on"
 L["Debuging are now turned off"] = "Debuging are now turned off"
 L["Available commands:"] = "Available commands:"
-L["/at set <earth/fire/water/air> # - Sets the totems shown of that element to #."] = "/at set <earth/fire/water/air> # - Sets the totems shown of that element to #."
-L["/at direction <up/down> - Set the direction totems pop up."] = "/at direction <up/down> - Set the direction totems pop up."
-L["/at order <element 1, element 2, element 3, element 4> - Sets the order of the totems, from left to right."] = "/at order <element 1, element 2, element 3, element 4> - Sets the order of the totems, from left to right."
-L["/at scale # - Sets the scale of ArchiTotem, default is 1."] = "/at scale # - Sets the scale of ArchiTotem, default is 1."
-L["/at showall - Toggles show all mode, displaying all totems on mouseover."] = "/at showall - Toggles show all mode, displaying all totems on mouseover."
-L["/at bottomcast - Toggles moving totems to the bottom line when cast"] = "/at bottomcast - Toggles moving totems to the bottom line when cast"
+L["/at set <earth/fire/water/air> # - Sets the totems shown of that element to #."] =
+"/at set <earth/fire/water/air> # - Sets the totems shown of that element to #."
+L["/at direction <up/down> - Set the direction totems pop up."] =
+"/at direction <up/down> - Set the direction totems pop up."
+L["/at order <element 1, element 2, element 3, element 4> - Sets the order of the totems, from left to right."] =
+"/at order <element 1, element 2, element 3, element 4> - Sets the order of the totems, from left to right."
+L["/at scale # - Sets the scale of ArchiTotem, default is 1."] =
+"/at scale # - Sets the scale of ArchiTotem, default is 1."
+L["/at showall - Toggles show all mode, displaying all totems on mouseover."] =
+"/at showall - Toggles show all mode, displaying all totems on mouseover."
+L["/at bottomcast - Toggles moving totems to the bottom line when cast"] =
+"/at bottomcast - Toggles moving totems to the bottom line when cast"
 L["/at timers - Toggles showing timers"] = "/at timers - Toggles showing timers"
 L["/at tooltip - Toggles showing tooltips"] = "/at tooltip - Toggles showing tooltips"
 L["/at debug - Toggles debuging"] = "/at debug - Toggles debuging"
@@ -60,7 +65,7 @@ L["Moving the bar:"] = "Moving the bar:"
 L["Ctrl-RightClick and Drag any of the main buttons"] = "Ctrl-RightClick and Drag any of the main buttons"
 L["Ordering totems of same element:"] = "Ordering totems of same element:"
 L["Ctrl-LeftClick any of the buttons"] = "Ctrl-LeftClick any of the buttons"
-L["Unavailable command. Type /at for help."] = "Unavailable command. Type /at for help."		
+L["Unavailable command. Type /at for help."] = "Unavailable command. Type /at for help."
 L["Elements must be written in english!"] = "Elements must be written in english!"
 L["Direction must be down or up!"] = "Direction must be down or up!"
 L["Scale must be a number!"] = "Scale must be a number!"
@@ -70,7 +75,7 @@ L["Cast Earth Totem"] = "Cast Earth Totem"
 L["Cast Fire Totem"] = "Cast Fire Totem"
 L["Cast Water Totem"] = "Cast Water Totem"
 L["Cast Air Totem"] = "Cast Air Totem"
-	
+
 BINDING_NAME_CAST_EARTH_TOTEM = L["Cast Earth Totem"]
 BINDING_NAME_CAST_FIRE_TOTEM = L["Cast Fire Totem"]
 BINDING_NAME_CAST_WATER_TOTEM = L["Cast Water Totem"]
--- Locale/Localization_ruRU.lua
+++ Locale/Localization_ruRU.lua
@@ -24,7 +24,7 @@ L["Windfury Totem"] = "Тотем неистовства ветра"
 L["Grace of Air Totem"] = "Тотем легкости воздуха"
 L["Nature Resistance Totem"] = "Тотем защиты от сил природы"
 L["Windwall Totem"] = "Тотем стены ветра"
-L["Sentry Totem"] = "Сторожевой тотем"	
+L["Sentry Totem"] = "Сторожевой тотем"
 
 L["ver."] = "вер."
 L["loaded"] = "загружен"
@@ -37,7 +37,8 @@ L["Direction set to: Up"] = "Направление установлено на:
 L["Order set to: "] = "Порядок установлен на: "
 L["Scale set to: "] = "Масштаб установлен на: "
 L["Showing all totems on mouseover"] = "Включено отображение всех тотемов при наведении курсора мыши"
-L["Showing only one element on mouseover"] = "Включено отображение тотемов только одной стихии при наведении курсора мыши"
+L["Showing only one element on mouseover"] =
+"Включено отображение тотемов только одной стихии при наведении курсора мыши"
 L["Totems will move the the bottom line when cast"] = "Тотемы будут перемещаться на панель"
 L["Totems will stay where they are when cast"] = "Тотемы останутся там, где они есть"
 L["Timers are now turned on"] = "Таймеры включены"
@@ -47,17 +48,24 @@ L["Tooltips are now turned off"] = "Подсказки выключены"
 L["Debuging are now turned on"] = "Отладка включена"
 L["Debuging are now turned off"] = "Отладка выключена"
 L["Available commands:"] = "Доступные команды:"
-L["/at set <earth/fire/water/air> # - Sets the totems shown of that element to #."] = "/at set <earth/fire/water/air> # - Устанавливает количество отображаемых тотемов в этой стихии, на #."
-L["/at direction <up/down> - Set the direction totems pop up."] = "/at direction <up/down> - Устанавливает направление тотемов."
-L["/at order <element 1, element 2, element 3, element 4> - Sets the order of the totems, from left to right."] = "/at order <стихия 1, стихия 2, стихия 3, стихия 4> - Устанавливает порядок тотемов слева направо."
-L["/at scale # - Sets the scale of ArchiTotem, default is 1."] = "/at scale # - Устанавливает масштаб ArchiTotem, по умолчанию 1."
-L["/at showall - Toggles show all mode, displaying all totems on mouseover."] = "/at showall - Вкл./Выкл. отображение всех стихий, отображающих все тотемы при наведении курсора мыши."
-L["/at bottomcast - Toggles moving totems to the bottom line when cast"] = "/at bottomcast - Вкл./Выкл. перемещение тотемов на панель"
+L["/at set <earth/fire/water/air> # - Sets the totems shown of that element to #."] =
+"/at set <earth/fire/water/air> # - Устанавливает количество отображаемых тотемов в этой стихии, на #."
+L["/at direction <up/down> - Set the direction totems pop up."] =
+"/at direction <up/down> - Устанавливает направление тотемов."
+L["/at order <element 1, element 2, element 3, element 4> - Sets the order of the totems, from left to right."] =
+"/at order <стихия 1, стихия 2, стихия 3, стихия 4> - Устанавливает порядок тотемов слева направо."
+L["/at scale # - Sets the scale of ArchiTotem, default is 1."] =
+"/at scale # - Устанавливает масштаб ArchiTotem, по умолчанию 1."
+L["/at showall - Toggles show all mode, displaying all totems on mouseover."] =
+"/at showall - Вкл./Выкл. отображение всех стихий, отображающих все тотемы при наведении курсора мыши."
+L["/at bottomcast - Toggles moving totems to the bottom line when cast"] =
+"/at bottomcast - Вкл./Выкл. перемещение тотемов на панель"
 L["/at timers - Toggles showing timers"] = "/at timers - Вкл./Выкл. отображение таймеров"
 L["/at tooltip - Toggles showing tooltips"] = "/at tooltip - Вкл./Выкл. отображение подсказок"
 L["/at debug - Toggles debuging"] = "/at debug - Вкл./Выкл. отладку"
 L["Moving the bar:"] = "Перемещение панели:"
-L["Ctrl-RightClick and Drag any of the main buttons"] = "Зажмите Ctrl+правую кнопку мыши и перетащите панель любую из основных кнопок"
+L["Ctrl-RightClick and Drag any of the main buttons"] =
+"Зажмите Ctrl+правую кнопку мыши и перетащите панель любую из основных кнопок"
 L["Ordering totems of same element:"] = "Порядок тотемов:"
 L["Ctrl-LeftClick any of the buttons"] = "Зажмите Ctrl+щелкните левой кнопкой мыши на любую из кнопок"
 L["Unavailable command. Type /at for help."] = "Недоступная команда. Введите /at для помощи."
